11f11a845596f1854648322e702665b165813ad9
Hudson S. Borges <hudsonsilbor@gmail.com>
2023-12-11 14:40:13 -0400

    test: adiciona testes ao navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,9 @@
 'use client';
 
-import {
-  Avatar,
-  Box,
-  Button,
-  IconButton,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-} from '@chakra-ui/react';
+import { Avatar, Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import classNames from 'classnames';
 import Link from 'next/link';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { MdAdminPanelSettings } from 'react-icons/md';
 import { useSessionContext } from 'supertokens-auth-react/recipe/session';
 
@@ -21,10 +12,13 @@ import Logo from './Logo';
 export default function Navbar(props: { className?: string }) {
   const context = useSessionContext();
 
-  const fullName = useMemo(() => {
-    return context.loading
-      ? '...'
-      : `${context.accessTokenPayload.first_name} ${context.accessTokenPayload.last_name}`;
+  const userName = useMemo(() => {
+    return (
+      (!context.loading &&
+        context.doesSessionExist &&
+        `${context.accessTokenPayload.first_name} ${context.accessTokenPayload.last_name}`) ||
+      undefined
+    );
   }, [context]);
 
   return (
@@ -39,16 +33,18 @@ export default function Navbar(props: { className?: string }) {
       </Link>
       {!context.loading && context.doesSessionExist ? (
         <div className="flex items-center gap-8">
-          <Link href={'/admin'}>
-            <span className="flex gap-1 text-white">
-              <MdAdminPanelSettings className="text-2xl" /> Admin
-            </span>
-          </Link>
+          {context.accessTokenPayload?.roles?.includes('admin') && (
+            <Link href={'/admin'}>
+              <span data-testid="admin-btn" className="flex gap-1 text-white">
+                <MdAdminPanelSettings className="text-2xl" /> Admin
+              </span>
+            </Link>
+          )}
           <Menu>
-            <MenuButton>
+            <MenuButton data-testid="menu-btn">
               <Avatar
                 size={'md'}
-                name={fullName}
+                name={userName}
                 bgColor="primary-alt"
                 color="white"
                 fontWeight={'bold'}
@@ -62,7 +58,7 @@ export default function Navbar(props: { className?: string }) {
           </Menu>
         </div>
       ) : (
-        <Button size={'sm'} color="primary-alt" as={Link} href={'/login'}>
+        <Button data-testid="login-btn" size={'sm'} color="primary-alt" as={Link} href={'/login'}>
           Entrar
         </Button>
       )}
